e4819ba0d86c46cc8bc161762c1833f5e9839474
mdshap <notunmdshaptarshi@gmail.com>
2025-11-23 01:01:58 +0600

    added public APIs

--- index.js
+++ index.js
@@ -1,18 +1,74 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('User Server Is Available')
-})
+app.use(cors());
+app.use(express.json());
 
+const uri =
+  "mongodb+srv://booksHaven:6fgST8PpMukfzMzE@cluster0.blss57h.mongodb.net/?appName=Cluster0";
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.listen(port, ()=>{
-    console.log('User server is started on port:', port);
-    
-})
+app.get("/", (req, res) => {
+  res.send("User Server Is Available");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected");
+
+    const db = client.db("booksHavenDB");
+    const booksCollection = db.collection("books");
+
+    app.get("/books", async (req, res) => {
+      const books = booksCollection.find();
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.get("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await booksCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/popular-books", async (req, res) => {
+      const books = booksCollection.find().sort({ rating: -1 }).limit(6);
+      const result = await books.toArray();
+      res.send(result);
+    });
+
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      const result = await booksCollection.insertOne(newBook);
+      res.send(result);
+    });
+
+    app.get("/my-books", async (req, res) => {
+      res.send("mybooks");
+    });
+
+    app.post("/my-books", async (req, res) => {
+      res.send("mybooks");
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log("User server is started on port:", port);
+});
